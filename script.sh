b7dba2a448069040369ca96d68bc2255520dd3c5
Alexey Sobinov <alexey.sobinov@lykke.com>
2019-05-13 23:52:00 +0500

    Version updated and install script changed

--- script.sh
+++ script.sh
@@ -1,10 +1,15 @@
-cp node-exporter.service /lib/systemd/system/node-exporter.service
-mkdir /etc/node-exporter
-cp node_exporter /etc/node-exporter
+#!/bin/bash
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.18.0/node_exporter-0.18.0.linux-amd64.tar.gz
+tar -xvf node_exporter-0.18.0.linux-amd64.tar.gz
+sudo mv node_exporter-0.18.0.linux-amd64/node_exporter /usr/local/bin/
+sudo useradd -rs /bin/false node_exporter
 
-sudo systemctl enable node-exporter.service
-sudo systemctl start node-exporter.service
+sudo cp node_exporter.service /etc/systemd/system/node_exporter.service
+
+sudo systemctl daemon-reload 
+sudo systemctl enable node_exporter.service
+sudo systemctl start node_exporter.service
 
 echo "service has been added and activated"
 
-sudo systemctl status node-exporter.service
+sudo systemctl status node_exporter.service
